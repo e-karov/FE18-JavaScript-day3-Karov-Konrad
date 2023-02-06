217aa173dea6215191736137b992d6384dfe99e2
Emanoil Karov <e_karov@yahoo.com>
2023-02-06 17:09:57 +0100

    Add 'intermediate ex 3'- solution with nested loops

--- Karov/intermediate/ex_3.js
+++ Karov/intermediate/ex_3.js
@@ -1,4 +1,16 @@
+// Solution with the build in String method String.repeat():
+
+// for (let index = 1; index < 7; index++) {
+//     let row = "*".repeat(index);
+//     console.log(row);
+// }
+
+// Solution with nested loops:
+
 for (let index = 1; index < 7; index++) {
-    let row = "*".repeat(index);
+    let row ="";
+    for (let k = 1; k <= index; k++) {
+        row += "*";
+    }
     console.log(row);
 }
